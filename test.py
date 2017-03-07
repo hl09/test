6aaf8776e8910fa4f84edb3eb525c6f555aa496b
root <smk09@126.com>
2017-03-07 13:30:47 +0000

    new

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-for i in range(1,15):
-    print(str(i))
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+
+while True :
+    GPIO.output(11,True)
+    time.sleep(1)
+    GPIO.output(11,False)
+    time.sleep(1)
