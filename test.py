8a1a9d20ea50edecdcdc6ae257af8cd996727f3a
root <smk09@126.com>
2017-04-21 08:40:41 +0000

    new

--- test.py
+++ test.py
@@ -2,8 +2,16 @@
 import RPi.GPIO as GPIO
 import time
 
+
+pin_4 = 4
+pin_17 = 17
+pin_23 = 23
+pin_24 = 24
+
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
+
 
 
 
@@ -16,10 +24,33 @@ def turnLedOff():
     print('off')
 
 def turnLedFlash():
-    while True:
+    for i in range(1,10):
         GPIO.output(11, True)
         print('on')
         time.sleep(0.1)
         GPIO.output(11, False)
         print('off')
         time.sleep(0.1)
+
+
+def motorInit():
+    GPIO.setup(pin_4,GPIO.OUT)
+    GPIO.setup(pin_17, GPIO.OUT)
+    GPIO.setup(pin_23, GPIO.OUT)
+    GPIO.setup(pin_24, GPIO.OUT)
+
+def setStep(w1,w2,w3,w4):
+    GPIO.output(pin_4,w1)
+    GPIO.output(pin_17, w2)
+    GPIO.output(pin_23, w3)
+    GPIO.output(pin_24, w4)
+
+def forward(delay):
+    setStep(1,0,0,0)
+    time.sleep(delay)
+    setStep(0, 1, 0, 0)
+    time.sleep(delay)
+    setStep(0,0,1,0)
+    time.sleep(delay)
+    setStep(0,0,0,1)
+    time.sleep(delay)
