8346faa3da41953f4d4135c2c9a85be698bf2d39
hl <smk09@126.com>
2017-06-18 03:55:56 +0000

    modify get video method,use the shell command to convert format

--- cameraTools.py
+++ cameraTools.py
@@ -1,25 +1,33 @@
-#coding = utf-8
 
-import datetime
-from time import sleep
 import picamera
-
-
+import os
 
 
 def getPicture():
+  if os.path.exists("a.jpg"):
+    os.remove("a.jpg")
+
   camera = picamera.PiCamera()
   camera.capture('a.jpg')
-  camera.start_preview()
+  camera.close()
 
 
 def getVideo():
+    if os.path.exists('myvideo.h264'):
+      os.remove('myvideo.h264')
+
+    if os.path.exists('myvideo.mp4'):
+      os.remove('myvideo.mp4')
+
     camera = picamera.PiCamera()
+    camera.resolution = (1024,768)
+
     camera.start_preview()
-    camera.start_recording('video.h264')
-    sleep(6)
+    camera.start_recording('myvideo.h264')
+    camera.wait_recording(6)
     camera.stop_recording()
+    camera.close()
 
-getPicture()
+    os.system("MP4Box -add myvideo.h264 myvideo.mp4")
 
-getVideo()
+
--- wxTest.py
+++ wxTest.py
@@ -1,5 +1,6 @@
 #coding=utf8
 import itchat
+
 import test
 import cameraTools
 from itchat.content import *
@@ -27,6 +28,9 @@ def engineOff():
 def photo():
     cameraTools.getPicture()
 
+def video():
+    cameraTools.getVideo()
+
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING,PICTURE])
 
 def text_reply(msg):
@@ -52,13 +56,22 @@ def text_reply(msg):
 
     #处理拍照指令，将拍照图片保存且发送给指令者
     if msg['Text'] == 'photo':
-      itchat.send('%s: %s' % (msg['Type'], '执行指令'+msg['Text']), msg['FromUserName'])
+      itchat.send('%s: %s' % (msg['Type'], msg['Text'] + ' processed'), msg['FromUserName'])
       photo()
       print(sys.path[0] + "/a.jpg")
       itchat.send_image(sys.path[0] + "/a.jpg", msg['FromUserName'])
 
+
+
+    if msg['Text'] == 'video':
+      itchat.send('%s: %s' % (msg['Type'], msg['Text'] + ' processed'), msg['FromUserName'])
+      video()
+      print(sys.path[0] + "/myvideo.h264")
+      itchat.send_file(sys.path[0] + "/myvideo.mp4", msg['FromUserName'])
+
+
     if msg['Text'] == 'water':
-      itchat.send('%s: %s' % (msg['Type'], '执行指令'+msg['Text']), msg['FromUserName'])
+      itchat.send('%s: %s' % (msg['Type'], msg['Text'] + ' processed'), msg['FromUserName'])
       engineStart()
       time.sleep(8)
       photo()
